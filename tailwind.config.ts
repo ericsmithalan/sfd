c32459e85657efd0f2796c76e67fd47c583dce1c
Eric Smith <546067+ericsmithalan@users.noreply.github.com>
2024-08-24 05:30:00 -0400

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
+    safelist: ["flex", "flex-col", "flex-row"],
     theme: {
         container: {
             screens: {
