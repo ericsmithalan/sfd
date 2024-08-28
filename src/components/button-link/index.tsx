7b815c1397340332d1a6f0a51ecc20280b23192d
Eric Smith <546067+ericsmithalan@users.noreply.github.com>
2024-08-28 08:24:04 -0400

    updates

--- src/components/button-link/index.tsx
+++ src/components/button-link/index.tsx
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 import Link from "next/link";
-import { IconNames } from "../icon/types";
+import { IconNames } from "../icon";
 import { IconHelper } from "../icon/helper";
 
 interface ButtonLinkProps {
