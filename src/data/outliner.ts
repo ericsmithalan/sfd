ae1b994da6e63573e20c60266e4c150bdd5a8499
Eric Smith <546067+ericsmithalan@users.noreply.github.com>
2025-01-21 14:06:24 -0500

    updates

--- src/data/outliner.ts
+++ src/data/outliner.ts
@@ -102,122 +102,40 @@ export const rootOutliner: Array<IOutliner> = [
             },
         ],
     },
+    {
+        id: 653,
+        name: "Sideboards",
+        level: 1,
+        children: [
+            {
+                id: 4334,
+                level: 2,
+                name: "Sideboard 1",
+                imageResouce: {
+                    primaryImg: 10,
+                    count: 10,
+                    path: "/models/case-1/images/",
+                },
+                children: [
+                    {
+                        id: 10984,
+                        modelUrl: "/models/case-1/final.glb",
+                        level: 3,
+                        name: "Final Model",
+                        children: [],
+                    },
+                    {
+                        id: 5342,
+                        modelUrl: "/models/case-1/build.glb",
+                        level: 3,
+                        name: "Build",
+                        children: [],
+                    },
+                ],
+            },
+        ],
+    },
 ];
-// export const rootOutliner: Array<IOutliner> = [
-//     {
-//         id: 11000,
-//         name: "Bunk Beds",
-//         level: 1,
-//         imageResouce: {
-//             primaryImg: 18,
-//             count: 18,
-//             path: "/models/bunks/images/",
-//         },
-//         children: [
-//             {
-//                 id: 11001,
-//                 level: 2,
-//                 modelUrl: "/models/bunks/bunks1.glb",
-//                 name: "Final Model",
-//                 children: [],
-//             },
-//             {
-//                 id: 11002,
-//                 modelUrl: "/models/bunks/mockups.glb",
-//                 level: 2,
-//                 name: "Initial Mockups",
-//                 children: [],
-//             },
-//         ],
-//     },
-//     {
-//         id: 11003,
-//         name: "Case",
-//         level: 1,
-//         imageResouce: {
-//             primaryImg: 10,
-//             count: 10,
-//             path: "/models/case-1/images/",
-//         },
-//         children: [
-//             {
-//                 modelUrl: "/models/case-1/final.glb",
-//                 level: 3,
-//                 id: 11004,
-//                 name: "Final Model",
-//                 children: [],
-//             },
-//             {
-//                 modelUrl: "/models/case-1/build.glb",
-//                 level: 3,
-//                 id: 11005,
-//                 name: "Build Steps",
-//                 children: [],
-//             },
-//         ],
-//     },
-//     {
-//         id: 11006,
-//         name: "Desk",
-//         level: 1,
-//         imageResouce: {
-//             primaryImg: 1,
-//             count: 3,
-//             path: "/models/desk/images/",
-//         },
-//         children: [
-//             {
-//                 modelUrl: "/models/desk/desk1.glb",
-//                 level: 2,
-//                 id: 11007,
-//                 name: "Final Model",
-//                 children: [],
-//             },
-//         ],
-//     },
-//     {
-//         id: 11023,
-//         name: "Desk 2",
-//         level: 1,
-//         children: [
-//             {
-//                 modelUrl: "/models/desk-2/desk-2.glb",
-//                 level: 2,
-//                 id: 11022,
-//                 name: "Final Model",
-//                 children: [],
-//             },
-//         ],
-//     },
-//     {
-//         id: 11323,
-//         name: "Desk 3",
-//         level: 1,
-//         children: [
-//             {
-//                 modelUrl: "/models/desk-3/desk-3.glb",
-//                 level: 2,
-//                 id: 11322,
-//                 name: "Final Model",
-//                 children: [],
-//             },
-//         ],
-//     },
-//     {
-//         id: 12323,
-//         name: "Case 2",
-//         level: 1,
-//         children: [
-//             {
-//                 modelUrl: "/models/john/case-1.glb",
-//                 level: 2,
-//                 id: 12322,
-//                 name: "Final Model",
-//                 children: [],
-//             },
-//         ],
-//     },
-// ];
 
 export const outlinerIdToName = (
     outliner: Array<IOutliner> = rootOutliner,
