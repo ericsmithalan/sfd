de6b2f3ee6538c0ceb85bffb3998537da83b38be
Eric Smith <546067+ericsmithalan@users.noreply.github.com>
2026-08-31 12:20:21 -0400

    big fixes from previous checkin

--- src/components/panel/index.tsx
+++ src/components/panel/index.tsx
@@ -1,9 +1,8 @@
 import clsx from "clsx";
-import { createRef, FC, ReactNode, RefObject, useEffect, useState } from "react";
+import { createRef, FC, ReactNode, RefObject } from "react";
 import { IOutliner } from "../../interface";
 import { IconName } from "../../types";
 import { Button } from "../button";
-import { NavLink } from "../nav-link";
 import { BorderHighlight } from "./BorderHighlight";
 import "./style.scss";
 
@@ -28,22 +27,10 @@ export const Panel: FC<PanelProps> = ({
     ref,
     selected,
 }) => {
-    const [open, setOpen] = useState(true);
     const panelRef = ref || createRef();
 
-    useEffect(() => {
-        if (selected) {
-            setOpen(false);
-        } else {
-            setOpen(opened);
-        }
-    }, [opened, selected]);
-
     return (
-        <div
-            ref={panelRef}
-            className={clsx("panel", open ? "panel-open" : "panel-closed", className)}
-        >
+        <div ref={panelRef} className={clsx("panel", className)}>
             {title && (
                 <Button
                     iconFill={true}
@@ -51,37 +38,11 @@ export const Panel: FC<PanelProps> = ({
                     variant="panel"
                     icon={icon}
                     text={title}
-                    onClick={() => {
-                        setOpen(!open);
-                    }}
                 />
             )}
 
-            {!open && selected && (
-                <NavLink
-                    variant="outliner"
-                    href={``}
-                    icon="armchair"
-                    active={true}
-                    className="selected"
-                    text={selected.name}
-                    onClick={(e) => {
-                        e.preventDefault();
-                    }}
-                />
-            )}
-            {open && <div className={clsx("panel-content", contentCss)}>{children}</div>}
-            {!open && (
-                <Button
-                    onClick={() => {
-                        setOpen(!open);
-                    }}
-                    variant="back"
-                    text={open ? "show less" : "show all"}
-                    className="btn-expand"
-                    icon={open ? "arrow-up-s" : "arrow-down-s"}
-                />
-            )}
+            <div className={clsx("panel-content", contentCss)}>{children}</div>
+
             <div className="inner-border"></div>
             <BorderHighlight />
         </div>
